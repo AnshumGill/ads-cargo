ee4aeb6071d537d400e5f4a216ca49fb963b35be
AnshumGill <anshumgill@gmail.com>
2020-09-06 15:50:16 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './navbar/banner/banner.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { QuoteComponent } from './quote/quote.component';
+import { ServicesComponent } from './services/services.component';
+import { IconsModule } from './icons/icons.module';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    JumbotronComponent,
+    QuoteComponent,
+    ServicesComponent,
+    ContactComponent,
+    FooterComponent,
+    TestimonialsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
